14691c3079b7934894602d68b05ae22d5af7cee5
coolfish <461836324@qq.com>
2015-01-12 20:48:32 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -24,7 +24,14 @@ $(function(){
 	/* 选中page */
 	$('.left-menu ul').on('click', 'li', function(){
 		var _this = $(this);
+		console.log(333);
 		on_page(_this);
+	});
+	/* 删除page */
+	$('.left-menu ul').on('click', '.left-menu-panel-close', function(){
+		var _this = $(this).parent().parent();
+		del_page(_this);
+		return false;
 	});
 
 
@@ -40,8 +47,9 @@ $(function(){
 //_______________page
 // 新增page
 function add_page(){
+	$('.left-menu ul li .left-menu-panel').removeClass('on');
 	var _num = $('.left-menu ul li').length + 1;
-	var _html = '<li><div class="left-menu-panel"><div class="left-menu-panel-title">' + _num + '</div><div class="left-menu-panel-content"></div></div></li>';
+	var _html = '<li><div class="left-menu-panel on"><div class="left-menu-panel-title">' + _num + '</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
 	$('.left-menu ul').append(_html);
 }
 // 选中page
@@ -50,3 +58,26 @@ function on_page(obj){
 	obj.children('.left-menu-panel').addClass('on');
 }
 //删除page
+function del_page(obj){
+	var _parent = obj.parent().find('li');
+	var _length = _parent.length;
+	if(_length > 1){
+		//删除
+		var _index = obj.index();//当前是第几个
+		for(var i = _index; i < _length; i++){
+			_parent.eq(i).find('.left-menu-panel-title').html(i);
+		};
+		if(_index == _length - 1){
+			on_page(obj.prev());//点亮前一个
+		}else{
+			on_page(obj.next());//点亮后一个
+		}
+		obj.remove();//移出去
+	}else{
+		//清空
+		obj.find('.left-menu-panel-content').empty();
+	}
+}
+
+
+
