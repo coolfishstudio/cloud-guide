faa6e176aca6bfd3e2b955a0683f882f63e628a7
coolfish <461836324@qq.com>
2015-01-13 13:10:31 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
 $(function(){
-
-
 	var typeBtn = {
 		'imgType' : ['jc','ys','pl','gs','xg'],
 		'textType' : ['jc','ys','pl','gs','xg'],
@@ -23,72 +21,82 @@ $(function(){
 	});
 
 
+
 	/* 新增page */
 	$('.add-page-btn').click(function(){
-		add_page();
+		pageObj.add();
 	});
 	/* 选中page */
 	$('.left-menu ul').on('click', 'li', function(){
 		var _this = $(this);
-		on_page(_this);
+		pageObj.on(_this);
 	});
 	/* 删除page */
 	$('.left-menu ul').on('click', '.left-menu-panel-close', function(){
 		var _this = $(this).parent().parent();
-		del_page(_this);
+		pageObj.del(_this);
 		return false;
 	});
 
 
 	/* 初始化 */
 	function init(){
-		//固定left-menu的高度
-		var _height = $(window).height() - 102;
-		$('.left-menu').css('height',_height);
+		pageObj.init();
 	}
 	init();
 });
 
 //_______________page
-// 新增page
-function add_page(){
-	$('.left-menu ul li .left-menu-panel').removeClass('on');
-	var _num = $('.left-menu ul li').length + 1;
-	var _html = '<li><div class="left-menu-panel on"><div class="left-menu-panel-title">' + _num + '</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
-	$('.left-menu ul').append(_html);
-	$('.left-menu').animate({scrollTop : $('.left-menu').outerHeight()},500);
-}
-// 选中page
-function on_page(obj){
-	$('.left-menu ul li .left-menu-panel').removeClass('on');
-	obj.children('.left-menu-panel').addClass('on');
-}
-//删除page
-function del_page(obj){
-	var _parent = obj.parent().find('li');
-	var _length = _parent.length;
-	if(_length > 1){
-		//删除
-		var _index = obj.index();//当前是第几个
-		for(var i = _index; i < _length; i++){
-			_parent.eq(i).find('.left-menu-panel-title').html(i);
-		};
-		if(_index == _length - 1){
-			on_page(obj.prev());//点亮前一个
+var pageObj = {//obj is li
+	//固定left-menu的高度
+	init : function(){
+		var _height = $(window).height() - 102;
+		$('.left-menu').css('height',_height);
+	}
+	//新增
+	add : function(){
+		$('.left-menu ul li .left-menu-panel').removeClass('on');
+		var _num = $('.left-menu ul li').length + 1;
+		var _html = '<li><div class="left-menu-panel on"><div class="left-menu-panel-title">' + _num + '</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
+		$('.left-menu ul').append(_html);
+		$('.left-menu').animate({scrollTop : $('.left-menu').get(0).scrollHeight},100);
+	},
+	//选中
+	on : function(obj){
+		$('.left-menu ul li .left-menu-panel').removeClass('on');
+		obj.children('.left-menu-panel').addClass('on');
+	},
+	//删除
+	del : function(obj){
+		var _parent = obj.parent().find('li');
+		var _length = _parent.length;
+		if(_length > 1){
+			//删除
+			var _index = obj.index();//当前是第几个
+			for(var i = _index; i < _length; i++){
+				_parent.eq(i).find('.left-menu-panel-title').html(i);
+			};
+			if(_index == _length - 1){
+				pageObj.on(obj.prev());//点亮前一个
+			}else{
+				pageObj.on(obj.next());//点亮后一个
+			}
+			obj.remove();//移出去
 		}else{
-			on_page(obj.next());//点亮后一个
+			//清空
+			obj.find('.left-menu-panel-content').empty();
 		}
-		obj.remove();//移出去
-	}else{
-		//清空
+	},
+	//拖拽
+	drag : function(obj){
+
+	},
+	//清空
+	clear : function(obj){
 		obj.find('.left-menu-panel-content').empty();
 	}
-}
-
-//_______________menu
-function getRightMenu(type){
-
-}
+};
+//_______________
 
 
 
