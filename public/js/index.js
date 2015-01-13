e4fa6aa2eeb9d1bbbf95abe516b3c7da95f4436d
coolfish <461836324@qq.com>
2015-01-13 22:55:44 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,32 @@
 $(function(){
-	var typeBtn = {
-		'imgType' : ['jc','ys','pl','gs','xg'],
-		'textType' : ['jc','ys','pl','gs','xg'],
-		'btnType' : ['jc','ys','pl','gs','xg'],
-		'musicType' : ['yy'],
-		'pageType' : ['jc','mb']
-	};
 
-	/* 头部新增按钮 */
-	$('.top-control-box').on('click', '.top-control', function(){
-		console.log($(this).attr('relName'));
-		var _this = $(this);
-		$('.right-menu ul li').css('display','none');
-		for (var i = 0; i < typeBtn[$(this).attr('relName')].length; i++) {
-			console.log(typeBtn[$(this).attr('relName')][i]);
-			$('.right-menu ul li').eq(i).removeClass('on');
-			$('.right-menu ul li[relName='+typeBtn[$(this).attr('relName')][i]+']').css('display','block');
-		};
-		$('.right-menu ul li[relName='+typeBtn[$(this).attr('relName')][0]+']').addClass('on');
+
+	//--------页面按钮
+	/* 用户按钮 */
+	$('#userBtn').click(function(){
+		var _width = $('.right-bar').width();
+		$('.right-bar').stop().animate({'right' : -_width}, 500, function(){
+			$('#menuBtn').css('display','block');
+			$('#userBtn').css('display','none');
+			$('.user-bar').stop().animate({'right' : 0}, 500);
+		});
+	});
+	/* 菜单按钮 */
+	$('#menuBtn').click(function(){
+		var _width = $('.user-bar').width();
+		$('.user-bar').stop().animate({'right' : -_width}, 500, function(){
+			$('#userBtn').css('display','block');
+			$('#menuBtn').css('display','none');
+			$('.right-bar').stop().animate({'right' : 0}, 500);
+		});
+	});
+	/* 新增文字按钮 */
+	$('#add-text').click(function(){
+
 	});
 
 
-
+	
 	/* 新增page */
 	$('.add-page-btn').click(function(){
 		pageObj.add();
@@ -38,7 +43,6 @@ $(function(){
 		return false;
 	});
 
-
 	/* 初始化 */
 	function init(){
 		cardObj.init();
@@ -51,10 +55,6 @@ $(function(){
 var cardObj = {
 	//初始化
 	init : function(){
-		if(document.location.pathname != localStorageObj.find('card_current')){
-			localStorageObj.clear();
-			localStorageObj.add('card_current', document.location.pathname);
-		}
 	}
 };
 //_______________page
@@ -63,31 +63,22 @@ var pageObj = {//obj is li
 	init : function(){
 		var _height = $(window).height() - 102;
 		$('.left-menu').css('height',_height);
-		if(!localStorageObj.find('page_num')){
-			localStorageObj.add('page_num', 1);
-			localStorageObj.add('page_current', 0);
-		}
-		var pageNum = localStorageObj.find('page_num');
-		for (var i = 0; i < pageNum; i++) {
-			var _html = '<li><div class="left-menu-panel' + (localStorageObj.find('page_current') == i ? ' on' : '') + '"><div class="left-menu-panel-title">' + (i + 1) + '</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
-			$('.left-menu ul').append(_html);
-		};
+		var _html = '<li><div class="left-menu-panel on"><div class="left-menu-panel-title">1</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
+		$('.left-menu ul').append(_html);
 	},
 	//新增
 	add : function(){
+		console.log(11);
 		$('.left-menu ul li .left-menu-panel').removeClass('on');
 		var _num = $('.left-menu ul li').length + 1;
 		var _html = '<li><div class="left-menu-panel on"><div class="left-menu-panel-title">' + _num + '</div><div class="left-menu-panel-content"></div><div class="left-menu-panel-close">x</div></div></li>';
 		$('.left-menu ul').append(_html);
 		$('.left-menu').animate({scrollTop : $('.left-menu').get(0).scrollHeight},100);
-		localStorageObj.update('page_num', _num);
-		localStorageObj.update('page_current', _num - 1);
 	},
 	//选中
 	on : function(obj){
 		$('.left-menu ul li .left-menu-panel').removeClass('on');
 		obj.children('.left-menu-panel').addClass('on');
-		localStorageObj.update('page_current', obj.index());
 	},
 	//删除
 	del : function(obj){
@@ -105,9 +96,6 @@ var pageObj = {//obj is li
 				pageObj.on(obj.next());//点亮后一个
 			}
 			obj.remove();//移出去
-			var pageNum = localStorageObj.find('page_num');
-			localStorageObj.update('page_num', pageNum - 1);
-			localStorageObj.update('page_current', pageNum - 2);
 		}else{
 			//清空
 			obj.find('.left-menu-panel-content').empty();
@@ -118,29 +106,9 @@ var pageObj = {//obj is li
 
 	}
 };
-//_______________localStorage
-var localStorageObj = {
-	//存储
-	add : function(key, value){
-		localStorage[key] = value;
-	},
-	//修改
-	update : function(key, value){
-		localStorage[key] = value;
-	},
-	//清空
-	clear : function(){
-		localStorage.clear();
-	},
-	//删除
-	delete : function(key){
-		localStorage.removeItem(key);
-	},
-	//查找
-	find : function(key){
-		return localStorage[key];
+//_______________text
+var textObj = {
+	add : function(){
+
 	}
 };
-
-
-
